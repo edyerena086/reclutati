0406b78b25850077880f13287c832149602b3fcb
Administrador <edgar.yerena@metodika.mx>
2018-09-19 11:04:44 -0500

    Secure protocool middleware

---
+++ app/Http/Middleware/SecureProtocol.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ReclutaTI\Http\Middleware;
+
+use Closure;
+
+class SecureProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($this->app->environment('production') && !$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
