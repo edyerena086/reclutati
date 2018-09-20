c34dde2c998dd765f1470e7b19a3089f6039230a
Administrador <edgar.yerena@metodika.mx>
2018-09-20 18:05:34 -0500

    State seeder rebuild

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -14,17 +14,45 @@ class StatesTableSeeder extends Seeder
      */
     public function run()
     {
-        if (Storage::disk('seeder')->exists('estados.csv')) {
-        	$file = storage_path('app/seeder/estados.csv');
-        	$csv = Reader::createFromPath($file);
+        $states = [
+            [ 'name' => 'Aguascalientes' ],
+            [ 'name' => 'Baja California' ],
+            [ 'name' => 'Baja California Sur' ],
+            [ 'name' => 'Campeche' ],
+            [ 'name' => 'Coahuila' ],
+            [ 'name' => 'Colima' ],
+            [ 'name' => 'Chiapas' ],
+            [ 'name' => 'Chihuahua' ],
+            [ 'name' => 'Ciudad de México' ],
+            [ 'name' => 'Durango' ],
+            [ 'name' => 'Guanajuato' ],
+            [ 'name' => 'Guerrero' ],
+            [ 'name' => 'Hidalgo' ],
+            [ 'name' => 'Jalisco' ],
+            [ 'name' => 'Estado de México' ],
+            [ 'name' => 'Michoacán' ],
+            [ 'name' => 'Morelos' ],
+            [ 'name' => 'Nayarit' ],
+            [ 'name' => 'Nuevo León' ],
+            [ 'name' => 'Oaxaca' ],
+            [ 'name' => 'Puebla' ],
+            [ 'name' => 'Querétaro' ],
+            [ 'name' => 'Quintana Roo' ],
+            [ 'name' => 'San Luis Potosí' ],
+            [ 'name' => 'Sinaloa' ],
+            [ 'name' => 'Sonora' ],
+            [ 'name' => 'Tabasco' ],
+            [ 'name' => 'Tamaulipas' ],
+            [ 'name' => 'Tlaxcala' ],
+            [ 'name' => 'Veracruz' ],
+            [ 'name' => 'Yucatán' ],
+            [ 'name' => 'Zacatecas' ]
+        ];
 
-        	foreach ($csv as $row) {
-        		$stateName = strtolower($row[0]);
-
-        		if (State::where('name', $stateName)->get()->count() == 0) {
-        			State::create(['name' => utf8_encode($stateName)]);
-        		}
-        	}
+        foreach ($states as $state) {
+            if (State::where('name', $state['name'])->first() == null) {
+                State::create($state);
+            }
         }
     }
 }
