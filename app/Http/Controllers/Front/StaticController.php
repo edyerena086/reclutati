261064e5cf14bb04f83e14e97cafa8152bee0997
Administrador <edgar.yerena@metodika.mx>
2018-09-24 10:50:45 -0500

    Home working

--- app/Http/Controllers/Front/StaticController.php
+++ app/Http/Controllers/Front/StaticController.php
@@ -10,7 +10,7 @@ class StaticController extends Controller
 {
     public function index()
     {
-    	$vacancies = Vacancy::where('publish', true)->orderBy('created_at', 'DESC')->take(10)->get();
+    	$vacancies = Vacancy::where('publish', true)->orderBy('created_at', 'DESC')->with(['recruiter', 'state', 'jobType'])->take(10)->get();
 
     	return view('front.static.index', ['vacancies' => $vacancies]);
     }
